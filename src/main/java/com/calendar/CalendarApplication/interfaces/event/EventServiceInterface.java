67d34ecf27a40a0ed0d413014758a1065931cb71
gustavo <gugaxavier2005@gmail.com>
2025-03-18 16:57:37 -0300

    fix: adjust in EventServiceInterface

--- src/main/java/com/calendar/CalendarApplication/interfaces/event/EventServiceInterface.java
+++ src/main/java/com/calendar/CalendarApplication/interfaces/event/EventServiceInterface.java
@@ -2,6 +2,7 @@ package com.calendar.CalendarApplication.interfaces.event;
 
 import com.calendar.CalendarApplication.dtos.event.CreateEventDto;
 import com.calendar.CalendarApplication.dtos.event.GetEventsDto;
+import com.calendar.CalendarApplication.dtos.event.UpdateEventDto;
 import com.calendar.CalendarApplication.entity.Event;
 import com.calendar.CalendarApplication.entity.User;
 
@@ -12,4 +13,5 @@ public interface EventServiceInterface {
 
     public Event createEvent(CreateEventDto event, User user);
     public List<GetEventsDto> findEventsByUserId(long userId);
+    public Event updateEvent(UpdateEventDto eventToUpdate);
 }
