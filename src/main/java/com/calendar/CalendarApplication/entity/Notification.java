5c630a54f4692cdc01511ddfc821f5aa2ff0233c
gustavo <gugaxavier2005@gmail.com>
2025-03-25 16:59:33 -0300

    fix: adjust in notification id generation type

--- src/main/java/com/calendar/CalendarApplication/entity/Notification.java
+++ src/main/java/com/calendar/CalendarApplication/entity/Notification.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Notification {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @ManyToOne(targetEntity = Event.class)
