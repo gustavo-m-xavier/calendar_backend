005c1d45f5dfb7dc40c3faebe2bd0cfa24c3d49a
gustavo <gugaxavier2005@gmail.com>
2025-04-12 19:47:37 -0300

    enhancement: improve notification entity to receive more attributes

--- src/main/java/com/calendar/CalendarApplication/entity/Notification.java
+++ src/main/java/com/calendar/CalendarApplication/entity/Notification.java
@@ -10,6 +10,12 @@ public class Notification {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
     @ManyToOne(targetEntity = Event.class)
     @JoinColumn(referencedColumnName = "id")
     private Event eventId;
@@ -21,13 +27,31 @@ public class Notification {
     @Column(name = "has_seen", nullable = false)
     private boolean hasSeen;
 
-    public Notification(Event eventId, User userId, boolean hasSeen) {
+    public Notification(String title, String description, Event eventId, User userId, boolean hasSeen) {
+        this.title = title;
+        this.description = description;
         this.eventId = eventId;
         this.userId = userId;
         this.hasSeen = hasSeen;
     }
 
     public Notification() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Event getEventId() {
         return eventId;
