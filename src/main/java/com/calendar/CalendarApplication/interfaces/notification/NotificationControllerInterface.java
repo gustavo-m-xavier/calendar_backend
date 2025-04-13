e5381d050764ab6b774f90304b74f4eed803c3fa
gustavo <gugaxavier2005@gmail.com>
2025-04-13 12:09:14 -0300

    enhancement: add interface for notification controller and service

--- src/main/java/com/calendar/CalendarApplication/interfaces/notification/NotificationControllerInterface.java
+++ src/main/java/com/calendar/CalendarApplication/interfaces/notification/NotificationControllerInterface.java
@@ -1,4 +1,13 @@
 package com.calendar.CalendarApplication.interfaces.notification;
 
+import com.calendar.CalendarApplication.dtos.notification.NotificationToUpdateDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
 public interface NotificationControllerInterface {
+    ResponseEntity<?> updateNotification(
+            @RequestBody NotificationToUpdateDto notificationToUpdateDto,
+            @RequestHeader("Authorization") String authorizationHeader
+    );
 }
