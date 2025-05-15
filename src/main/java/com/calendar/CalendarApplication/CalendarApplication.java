622fdf81bce437567d05b2708b90e62a569b4482
gustavo <gugaxavier2005@gmail.com>
2025-05-15 17:14:27 -0300

    feat: add websocket to main app

--- src/main/java/com/calendar/CalendarApplication/CalendarApplication.java
+++ src/main/java/com/calendar/CalendarApplication/CalendarApplication.java
@@ -2,12 +2,29 @@ package com.calendar.CalendarApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @SpringBootApplication
-public class CalendarApplication {
+@EnableWebSocketMessageBroker
+public class CalendarApplication implements WebSocketMessageBrokerConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalendarApplication.class, args);
 	}
 
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/calendar")
+				.setAllowedOrigins("*")
+				.withSockJS();
+	}
 }
