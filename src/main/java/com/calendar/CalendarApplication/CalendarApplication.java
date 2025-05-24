4c681f6757440a2eff64714a0fa483de36db9027
gustavo <gugaxavier2005@gmail.com>
2025-05-24 00:29:21 -0300

    update: start scheduled tasks implementation for due date notifications

--- src/main/java/com/calendar/CalendarApplication/CalendarApplication.java
+++ src/main/java/com/calendar/CalendarApplication/CalendarApplication.java
@@ -3,12 +3,14 @@ package com.calendar.CalendarApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @SpringBootApplication
 @EnableWebSocketMessageBroker
+@EnableScheduling
 public class CalendarApplication implements WebSocketMessageBrokerConfigurer {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/calendar/CalendarApplication/scheduling/ScheduledTasks.java
@@ -0,0 +1,7 @@
+package com.calendar.CalendarApplication.scheduling;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+}
