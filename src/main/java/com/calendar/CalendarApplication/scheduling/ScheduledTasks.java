32df9fa855a2672e04096365e5c2b7d94ee268ce
gustavo <gugaxavier2005@gmail.com>
2025-05-24 13:32:56 -0300

    update: add schedule test

--- src/main/java/com/calendar/CalendarApplication/scheduling/ScheduledTasks.java
+++ src/main/java/com/calendar/CalendarApplication/scheduling/ScheduledTasks.java
@@ -1,7 +1,22 @@
 package com.calendar.CalendarApplication.scheduling;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
 
 @Component
 public class ScheduledTasks {
+
+    private static final Logger logger = Logger.getLogger(ScheduledTasks.class.getName());
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    //schecule test
+    @Scheduled(fixedRate = 5000)
+    public void hourlyDueDateTask(){
+        logger.info(dateFormat.format(new Date()));
+    }
 }
